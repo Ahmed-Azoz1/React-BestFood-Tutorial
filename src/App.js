629a1683df634684175d2e3ecb89baaab0afc1d3
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2022-09-17 23:25:01 +0200

    New Project-BestFood-Tutorial

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import HeadlineCards from './components/HeadlineCards'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Food from './components/Food'
+import Category from './components/Category'
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <HeadlineCards/>
+      <Food/>
+      <Category/>
+    </div>
+  )
+}
+
+export default App
